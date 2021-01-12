8a62325d5e5f0b0480bd8a030903a734a557b596
tom1k <tomascosta@ua.pt>
2021-01-12 20:34:12 +0000

    Taking into account the ordered index

--- code/indexer.py
+++ code/indexer.py
@@ -102,34 +102,31 @@ class Indexer:
     def merge_blocks(self):
         self.temp_index = {}
         block_files = os.listdir(self.block_directory)
-        files = [open(self.block_directory+block_file) for block_file in block_files]
+        block_files = [open(self.block_directory+block_file) for block_file in block_files]
+        lines = [block_file.readline()[:-1] for block_file in block_files]
+        last_term = ""
+        index = 0
+
+        # deleting empty files
+        for block_file in block_files:
+            if lines[index] == "":
+                block_files.pop(index)
+                lines.pop(index)
+            else:
+                index += 1
 
         mem_initial = psutil.virtual_memory().available
         # start looking through words
-        while True:
-            # stopping condition
-            if len(files) == 0:
-                break
-            for file in files:
-                try:
-                    line = re.split(":",file.readline().rstrip('\n'),maxsplit=1)
-                except:
-                    continue
-                
-                # invalid lines
-                if line[0] == '':
-                    files.remove(file)
-                    continue
+        while len(block_files) > 0:
 
-                if line[0] in self.temp_index.keys():
-                    tmp_dict = self.temp_index[line[0]]['doc_ids']
-                    new_val = {**ast.literal_eval(line[1])['doc_ids'], **tmp_dict} # merging the two dicts
-                    self.temp_index[line[0]]['doc_ids'] = new_val
-                else: # we add to dict
-                    self.temp_index[line[0]] = ast.literal_eval(line[1])
-
+            min_index = lines.index(min(lines))
+            line = re.split(":",lines[min_index].rstrip('\n'),maxsplit=1)
+            current_term = line[0]
+            current_postings = line[1]
+            
+            # we check initially, so we dont put the same term in two diff files
             mem_used = mem_initial - psutil.virtual_memory().available 
-            if mem_used > 100000000: #only for cases bigger than 100Mb 
+            if mem_used > 100000000 and current_term!=last_term: #only for cases bigger than 100Mb 
                 ordered_dict = sorted(self.temp_index.items(), key = lambda kv: kv[0])
                 smallest_word = ordered_dict[0][0]
                 highest_word = ordered_dict[-1][0]
@@ -138,8 +135,25 @@ class Indexer:
                         string = f"{word}:{str(value)}\n"
                         f.write(string)
                 self.temp_index = {}
+                f.close()
                 mem_initial = psutil.virtual_memory().available
                 print("Memory used:", mem_used)
+
+            if current_term != last_term:
+                self.temp_index[current_term] = ast.literal_eval(current_postings)
+                last_term = current_term
+            else:
+                tmp_dict = self.temp_index[current_term]['doc_ids']
+                new_val = {**ast.literal_eval(current_postings)['doc_ids'], **tmp_dict} # merging the two dicts
+                self.temp_index[current_term]['doc_ids'] = new_val
+
+            lines[min_index] = block_files[min_index].readline()[:-1]
+
+            if lines[min_index] == "":
+                    block_files[min_index].close()
+                    block_files.pop(min_index)
+                    lines.pop(min_index)
+
 
         
         #print(self.temp_index['wild'])
